e33acb4a7601915c5e074d0dc14af7529ffa3dba
tejukul <tejaswini@fathomable.in>
2022-03-24 20:05:51 +0530

    2nd commit

--- newFile.js
+++ newFile.js
@@ -1,1 +1,34 @@
-console.log("New File");
+ console.log("New File");
+// console.log("New\nFile");
+// console.log(
+//     "New File"
+//     );
+// console.log("       New File");
+
+//////////////////////////
+
+// function printMsg(){
+//     console.log("printing msg");
+// }
+//  printMsg();
+//always use camelCase - recommended
+//JS is case sensitive
+
+//////////////////////////
+// Comminting - required for referencing
+
+//
+
+/*      */
+
+/**
+ * proffessional approach
+ * developers follows
+ * for documentation
+ * 
+ */
+
+//////////////////////////
+
+
+
